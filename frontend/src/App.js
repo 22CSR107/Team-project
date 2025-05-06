6f3dd62d7a0fa53971e914d227e79998e32c5366
Dhivya-26 <dhivyav.22ece@kongu.edu>
2025-05-06 12:03:18 +0530

    signup login update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 // Importing components
-
 import HomePage from './components/HomePage';
 import NavBar from './components/NavBar';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
+import SignupPage from './components/SignupPage';
+import LoginPage from './components/LoginPage';
+
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/signup" element={<SignupPage />} />   
+          <Route path="/login" element={<LoginPage />} />     
         </Routes>
       </div>
     </Router>
